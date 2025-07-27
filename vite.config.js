06f5e9a538012532269955ca0921272fa91f209c
aashishrawat6 <aashishrawat062001@gmail.com>
2025-07-27 08:46:21 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ARxDEV/", // Set the base path for GitHub Pages
   plugins: [tailwindcss(), react()],
 });
